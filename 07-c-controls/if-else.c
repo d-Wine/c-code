f693cfd239c8302dcaae07bd53195b42e5383f6a
d-Wine <dy98wine@gmail.com>
2020-11-26 19:02:48 +0630

    if-else statement

---
+++ 07-c-controls/if-else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a = 100;
+    if (a < 20){
+        printf("a is less than 20\n");
+    }
+    else
+    {
+        printf("a is greater than 20\n");
+    }
+    printf("value of a : %d\n",a);
+    return 0;
+}
