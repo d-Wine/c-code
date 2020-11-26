717c6ee14ac5dbbdb11bab9daf5dcf6c351ee911
d-Wine <dy98wine@gmail.com>
2020-11-26 19:03:18 +0630

    if-elseif statement

---
+++ 07-c-controls/if-elseif.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a = 100;
+    if (a == 10){
+        printf("Value of a is 10\n");
+    }else if(a == 20){
+        printf("Value of a is 20\n");
+    }else if(a == 30){
+        printf("Value of a is 30\n");
+    }
+    else{
+        printf("No Matching value");
+    }
+    return 0;
+}
